70a9ffbc39d30ef9d2209abc39355a21f6973683
danielquintaos <99265614+danielquintaos@users.noreply.github.com>
2023-02-08 13:09:19 -0300

    commit exercicios

--- exercicio1.py
+++ exercicio1.py
@@ -1,26 +1,14 @@
-#  Se achar necessario, faça import de outras bibliotecas
+contaPalavras = 0
+string = ['hello', 'world', 'world', 'how', 'are', 'you', 'doing', 'doing']
+conta = {}
+for word in string:
+   if word in conta :
+      conta[word] += 1
+   else:
+      conta[word] = 1
+len(conta)
 
+a = print(conta)
 
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
+def conta_palavras_unicas():
+    return a
--- exercicio2.py
+++ exercicio2.py
@@ -1,26 +1,23 @@
-#  Se achar necessario, faça import de outras bibliotecas
+def is_anagram(word):
+    frequency  = {}
+    for char in word:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    return frequency 
 
+print("Insira a primeira palavra:")
+a_word = input()
+print(" ")
+print("Insira a segunda palavra:")
+b_word = input()
+print(" ")
 
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
+if (len(a_word) != len(b_word)):
+   print ("False")
+else:
+    if (is_anagram(a_word) == is_anagram(b_word)):
+        print("True")
+    else:
+        print("False")
